65f0d6941b84170a673427e6b72080362180493d
Dmitrii Pavlov <git@venom.email>
2023-11-01 18:10:32 +0400

    pass event or call to handler

--- src/mock-transport.ts
+++ src/mock-transport.ts
@@ -29,10 +29,8 @@ export class TransportMock implements Transport {
     async run(subscribers: any) {
         const files = await fs.readdir(path.resolve(this.config.abiPath));
         const contracts: Contracts = {};
-        const anyMsgsBySender: {[key: string]: MessageEntry} = {};
-        const anyMsgsByReceiver: {[key: string]: MessageEntry} = {};
-        const nativeBySender: {[key: string]: MessageEntry} = {};
-        const nativeByReceiver: {[key: string]: MessageEntry} = {};
+        const msgsBySender: {[key: string]: MessageEntry} = {};
+        const msgsByReceiver: {[key: string]: MessageEntry} = {};
 
         files.forEach(async file => {
             if (path.extname(file) === '.json') {
@@ -53,31 +51,15 @@ export class TransportMock implements Transport {
                 filter.entries.forEach((entry) => {
                     if (entry.sender) {
                         if (typeof entry.sender === 'string') {
-                            anyMsgsBySender[entry.sender] = entry;
+                            msgsBySender[entry.sender] = entry;
                         } else if (typeof entry.sender === 'object') {
-                            anyMsgsBySender[entry.sender.address] = entry;
+                            msgsBySender[entry.sender.address] = entry;
                         }
                     } else if (entry.receiver) {
                         if (typeof entry.receiver === 'string') {
-                            anyMsgsByReceiver[entry.receiver] = entry;
+                            msgsByReceiver[entry.receiver] = entry;
                         } else if (typeof entry.receiver === 'object') {
-                            anyMsgsByReceiver[entry.receiver.address] = entry;
-                        }
-                    }
-                })
-            } else if (filter.type === 'native_transfer') {
-                filter.entries.forEach((entry) => {
-                    if (entry.sender) {
-                        if (typeof entry.sender === 'string') {
-                            nativeBySender[entry.sender] = entry;
-                        } else if (typeof entry.sender === 'object') {
-                            nativeBySender[entry.sender.address] = entry;
-                        }
-                    } else if (entry.receiver) {
-                        if (typeof entry.receiver === 'string') {
-                            nativeByReceiver[entry.receiver] = entry;
-                        } else if (typeof entry.receiver === 'object') {
-                            nativeByReceiver[entry.receiver.address] = entry;
+                            msgsByReceiver[entry.receiver.address] = entry;
                         }
                     }
                 })
@@ -95,28 +77,20 @@ export class TransportMock implements Transport {
                 const tx = transactions[txHash]
 
                 const decodedMsg = decodeMsg(contracts, tx.in_msg, 'message')
-                decodedMsg.flat().filter((item) => !!item).forEach((item) => subscribers[item.name](tx.in_msg))
+                decodedMsg.flat().filter((item) => !!item).forEach((item) => subscribers[item.name](tx.in_msg, item.call))
 
-                if (anyMsgsBySender[tx.in_msg.src]) {
-                    subscribers[anyMsgsBySender[tx.in_msg.src].name](tx.in_msg)
+                if (msgsBySender[tx.in_msg.src]) {
+                    subscribers[msgsBySender[tx.in_msg.src].name](tx.in_msg)
                 }
 
-                if (anyMsgsByReceiver[tx.in_msg.dst]) {
-                    subscribers[anyMsgsByReceiver[tx.in_msg.dst].name](tx.in_msg)
-                }
-
-                if (nativeBySender[tx.in_msg.src]) {
-                    subscribers[nativeBySender[tx.in_msg.src].name](tx.in_msg)
-                }
-
-                if (nativeByReceiver[tx.in_msg.dst]) {
-                    subscribers[nativeByReceiver[tx.in_msg.dst].name](tx.in_msg)
+                if (msgsByReceiver[tx.in_msg.dst]) {
+                    subscribers[msgsByReceiver[tx.in_msg.dst].name](tx.in_msg)
                 }
 
                 transactions[txHash].out_msgs.map((msg) => {
                     if (msg.msg_type_name === 'extOut') {
                         const decodedMsg = decodeMsg(contracts, msg, 'event')
-                        decodedMsg.flat().filter((item) => !!item).forEach((item) => subscribers[item.name](msg))
+                        decodedMsg.flat().filter((item) => !!item).forEach((item) => subscribers[item.name](msg, item.event))
                     }
                 })
             });
